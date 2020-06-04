8aa92bc4d51530084ba833228935217ed9ca2850
GrimLeo <leo40230@gmail.com>
2020-06-04 12:43:02 +0300

    git commite

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 require_once __DIR__.'/vendor/autoload.php';
 $test = new \HDESDK\SDKInit();
 
-echo $test->Auth('leo40230@gmail.com', 'da736d56-c09d-4c7f-96c3-d961c29b7ee6', '"https://grimleo.helpdeskeddy.com');
-
+$test->Auth('leo40230@gmail.com', 'da736d56-c09d-4c7f-96c3-d961c29b7ee6', '"https://grimleo.helpdeskeddy.com');
+echo $test->GetToken();
 
 
 
