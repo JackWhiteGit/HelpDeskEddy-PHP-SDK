7ee750c07b7fff578b6579dc74b2f2447c65beda
GrimLeo <leo40230@gmail.com>
2020-06-05 10:33:14 +0300

    git commite

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
-ini_set('display_errors', 1);
+
+//Package inclusion example
 
 require_once __DIR__.'/vendor/autoload.php';
+
+//Initialisation
 $test = new \HDESDK\SDKInit();
 
-$test->Auth('leo40230@gmail.com', 'da736d56-c09d-4c7f-96c3-d961c29b7ee6', 'https://grimleo.helpdeskeddy.com');
+//User authentication
+$test->Auth('example@example.com', 'your-api-key', 'https://example.helpdeskeddy.com');
 
+// Request examples
 $options = array(
     'ticket_id' => 3,
     'page'      => 1
 );
 
-print_r($test->CommentsGet($options));
+$answer = $test->CommentsGet($options);
 
 
 
