945196a1e8987140121a9839182f2e3d2c703be6
GrimLeo <leo40230@gmail.com>
2020-06-05 10:34:51 +0300

    git commite

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/vendor/autoload.php';
 //Initialisation
 $test = new \HDESDK\SDKInit();
 
-//User authentication
+//User authorization
 $test->Auth('example@example.com', 'your-api-key', 'https://example.helpdeskeddy.com');
 
 // Request examples
